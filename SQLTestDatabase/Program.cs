473fb4175e2e633a9b44dd7e4726a4aff526483b
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2020-09-24 16:47:00 -0500

    20200924: We have creating querys using linq and uploading images

--- SQLTestDatabase/Program.cs
+++ SQLTestDatabase/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Linq;
 
 namespace SQLTestDatabase
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            using (var context = new Context())
+            {
+
+                //El chofer que más viajes ha realizado en el último mes.
+                long TipoChofer = context.TipoPersona.Where(z => z.Nombre == "chofer").FirstOrDefault().Id;
+                long TipoCte = context.TipoPersona.Where(z => z.Nombre == "cliente").FirstOrDefault().Id;
+
+
+                var startOfTthisMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                var firstDay = startOfTthisMonth.AddMonths(-1);
+                var lastDay = startOfTthisMonth.AddDays(-1);
+
+                IQueryable<Viaje> viajesultimomes = context.Viaje.Where(x => x.FechaFin >= firstDay &&
+                      x.FechaFin <= lastDay);
+
+
+
+
+                //Los 5 clientes más lejanos
+                var items = context.Persona.Where(x=> x.TipoPersonaId == TipoCte).OrderByDescending(u => u.FechaCreacion).Take(5);
+
+
+                //Los 5 clientes frecuentes(con más pedidos hechos)
+                var frecuentes = context.PedidoCte.GroupBy(a => a.PersonaId).Where(grp => grp.Count() > 1).SelectMany(grp => grp.Select(r => r)).Take(5);
+
+                //Un listado de los últimos 10 pedidos, sabiendo de qué cliente son, qué chofer llevó el pedido y qué camioneta se utilizó.
+
+
+
+
+            }
+
         }
     }
 }
